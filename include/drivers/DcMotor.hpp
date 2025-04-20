87ab12399c09ccadbf05dc1182d5ae8a92330482
pab-h <dev.pab.2020@gmail.com>
2025-04-20 11:12:31 -0300

    feat: Driver do motor DC

---
+++ include/drivers/DcMotor.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace drivers {
+
+    /**
+     * É assumido que esteja usando uma ponte-H
+     */
+    
+    class DcMotor {
+
+        private:
+
+            u_int8_t en;
+
+            u_int8_t inl;
+            u_int8_t inr;
+
+        public:
+
+            DcMotor(u_int8_t en, u_int8_t inl, u_int8_t inr);
+
+            void clockwise();
+            void counterclockwise();
+            
+            void speed(u_int8_t pwm);
+            void stop();
+            
+    };
+
+}
---
+++ src/drivers/DcMotor.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "drivers/DcMotor.hpp"
+
+using namespace drivers;
+
+DcMotor::DcMotor(u_int8_t en, u_int8_t inl, u_int8_t inr) {
+
+    this->en  = en;
+    this->inl = inl;
+    this->inr = inr;
+
+    pinMode(this->en , OUTPUT);
+    pinMode(this->inl, OUTPUT);
+    pinMode(this->inr, OUTPUT);
+
+}
+
+void DcMotor::clockwise() {
+
+    digitalWrite(this->inl, HIGH);
+    digitalWrite(this->inr, LOW);
+
+}
+
+void DcMotor::counterclockwise() {
+
+    digitalWrite(this->inl, LOW);
+    digitalWrite(this->inr, HIGH);
+
+}
+
+void DcMotor::speed(u_int8_t pwm) {
+
+    analogWrite(this->en, pwm);
+
+}
+
+void DcMotor::stop() {
+
+    digitalWrite(this->inl, LOW);
+    digitalWrite(this->inr, LOW);
+
+}
