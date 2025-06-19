2fc815531080add933776dbf47e5bca64b3fd560
pab-h <dev.pab.2020@gmail.com>
2025-06-19 16:05:27 -0300

    fix: O uso dos testes unitários pelo plataformio parece interessante. Porém, necessita de demasiada configuração

--- test/test_blinkLed.cpp
+++ test/test_blinkLed.cpp
@@ -1,36 +1,16 @@
-#ifdef UNIT_TEST
-
 #include <Arduino.h>
-#include <unity.h>
-
-
-void test_blinkLed() {
-    
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    for(int i = 0; i < 3; i++) {
-
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(1000);
-
-    }
-
-    TEST_ASSERT_TRUE(true);
-
-}
 
 void setup() {
 
-    UNITY_BEGIN();
-
-    RUN_TEST(test_blinkLed);
-
-    UNITY_END();
+    pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
-void loop() {}
+void loop() {
 
-#endif
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+
+}
