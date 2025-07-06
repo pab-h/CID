b00101412b0d2ff62adef7281339d253eda18d1d
Ryam Lael <ryyamlael@gmail.com>
2025-07-05 22:27:06 -0300

    fix: solved conflicts

--- include/drivers/DcMotor.hpp
+++ include/drivers/DcMotor.hpp
@@ -21,13 +21,8 @@ namespace drivers {
 
         public:
 
-<<<<<<< HEAD
-            DcMotor(u_int8_t en, u_int8_t inl, u_int8_t inr);
-            //motor angle como parametro da função de rodar
-=======
             DcMotor(uint8_t en, uint8_t inl, uint8_t inr);
 
->>>>>>> feat/wifi-module
             void clockwise();
             void counterclockwise();
             
--- include/pins.hpp
+++ include/pins.hpp
@@ -47,12 +47,6 @@
 // #define IN3_PIN 0x77
 // #define IN4_PIN 0x77
 
-#define IN1_PIN 0x77 //apague
-#define IN2_PIN 0x77
-#define IN3_PIN 0x77
-#define IN4_PIN 0x77
-
-
 /* Rotary Encoder pins */
 
 #define SIGA_PIN 0x77
