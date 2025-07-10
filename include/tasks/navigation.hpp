9e8648af53357cfc92817eaabb6cd70c2b2ebc6c
pab-h <dev.pab.2020@gmail.com>
2025-07-10 15:14:30 -0300

    feat: Taref parcialmente feita para a notificação do sistema de medida

--- include/tasks/navigation.hpp
+++ include/tasks/navigation.hpp
@@ -4,5 +4,5 @@ namespace tasks {
 
     void vNavigationTask(void* pvParameters);
     void vUpdateRotaryEncoderTask(void* pvParameters);
-
+    void vNavigationNotificationsTask(void* pvParameters)
 }
--- src/tasks/navigation.cpp
+++ src/tasks/navigation.cpp
@@ -5,6 +5,19 @@
 using namespace application;
 using namespace drivers;
 
+void tasks::vNavigationNotificationsTask(void* pvParameters) {
+
+    Navigation*   navigation    = static_cast<Navigation*>(pvParameters);
+    Notifications notifications = navigation->getNotifications();
+
+    if (navigation->getState() == State::WAITING_MEASURE && !notifications.isMeasureSend) {
+        // envio auqi
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(50));
+
+}
+
 void tasks::vUpdateRotaryEncoderTask(void* pvParameters) {
     
     Navigation*    navigation = static_cast<Navigation*>(pvParameters);
